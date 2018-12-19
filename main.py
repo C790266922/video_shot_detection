de5ed92feeabce580d2c454c8846099b99185f84
C790266922 <790266922@qq.com>
2018-12-19 22:48:58 +0800

    add moment features diff

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 import cv2
 import os
 import matplotlib.pyplot as plt
+import argparse
 
 '''
 load image sequence from video
@@ -53,16 +54,127 @@ def threshold_hist_score(score, threshold = 10.0):
     print(key_frames)
     return key_frames
 
+'''
+calculate moment invariants feature difference between frames
+'''
+def get_mom_diff(frames):
+    mom_features = []
+    # traverse all frame and calculate moment feature
+    for frame in frames:
+        n20 = calc_n_pq(frame, 2, 0)
+        n02 = calc_n_pq(frame, 0, 2)
+        n11 = calc_n_pq(frame, 1, 1)
+        n30 = calc_n_pq(frame, 3, 0)
+        n03 = calc_n_pq(frame, 0, 2)
+        n12 = calc_n_pq(frame, 1, 2)
+        n21 = calc_n_pq(frame, 2, 1)
 
-if __name__ == "__main__":
-    # load frames
-    video_path = './movie.mp4'
-    frames = get_frames(video_path)
-    # calculate Hist score
+        # use 3 moment invarants to form a feature vector
+        f1 = n20 + n02
+        f2 = (n20 - n02) ** 2 + 4 * n11 ** 2
+        f3 = (n30 - 3 * n12) ** 2 + (3 * n21 - n03) ** 2
+        feat = [f1, f2, f3]
+
+        mom_features.append(np.array(feat))
+
+    # calculate feature difference between frames
+    mom_diffs = []
+    for i in range(1, len(mom_features)):
+        diff = np.sum((mom_features[i] - mom_features[i - 1]) ** 2) 
+        mom_diff.append(diff)
+
+    return mom_diffs
+
+'''
+calculate n_pq
+'''
+def calc_n_pq(frame, p, q):
+    mom = cv2.moments(frame)
+    r = 1 + (p + q) / 2
+    mean_x = mom['m10'] / mom['m00']
+    mean_y = mom['m01'] / mom['m00']
+
+    mean_x_mat = np.array(frame.size * [mean_x]).reshape(frame.shape)
+    mean_y_mat = np.array(frame.size * [mean_y]).reshape(frame.shape)
+
+    n_pq = 1 / (m ** r) * np.sum((frame - mean_x_mat) ** p *\
+            (frame - mean_y_mat) ** q * frame)
+
+    return n_pq
+
+'''
+threshold moment feature differences to get key frames
+'''
+def threshold_mom_diff(mom_diffs, threshold = 1.0):
+    key_frames = []
+
+    for i, diff in enumerate(mom_diffs):
+        if diff > threshold:
+            key_frames.append(i)
+
+    return key_frames
+
+'''
+use color histogram to get shot (start, end) frames
+'''
+def get_shot_hist(frames):
+    # calculate hist score
     score, hists = get_hist_score(frames)
     # plot score
     plt.figure(figsize = (8, 6))
     plt.plot(score)
     plt.savefig('score.png')
-    # threshold hist score
-    key_frames = threshold_hist_score(score)
+    # threshold score
+    shots = threshold_hist_score(score)
+    return shots
+
+'''
+use moment features to get shot (start, end) frames
+'''
+def get_shot_mom(frames):
+    # calculate moment feature differences
+    mom_diffs = get_mom_diff(frames)
+    print(mom_diffs)
+    print(np.mean(mom_diffs))
+    print(min(mom_diffs), max(mom_diffs))
+    # threshold diff
+    shots = threshold_mom_diff(mom_diffs)
+    print(shots)
+
+    return shots
+
+if __name__ == "__main__":
+    # load frames
+    video_path = './movie.mp4'
+    frames = get_frames(video_path)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--hist', action = 'store_true')
+    parser.add_argument('--moment', action = 'store_true')
+    parser.add_argument('--mix')
+
+    args = parser.parse_args()
+
+    # use color histogram to identify key frames
+    if args.hist:
+        shots = get_shot_hist(frames)
+
+    # use moment features to identify key frames
+    elif args.moment:
+        shots = get_shot_mom(frames)
+
+    # use both algorithm to get 2 key frames sets, then union or intersect
+    elif args.mix:
+        shots_hist = get_shot_hist(frames)
+        shots_mom = get_shot_mom(frames)
+        
+        shots = set()
+        # union
+        if args.mix == 1:
+            shots = set(shots_hist) | set(shots_mom)
+        # intersect
+        elif args.mix == 2:
+            shots = set(shots_hist) & set(shots_mom)
+        else:
+            printf("Wrong argument for --mix (1 or 2)")
+            exit()
